0f98a6e2ca3c79fe94480bc702ba636a97cddc80
GuyveK <guyve.khalili@gmail.com>
2019-02-17 15:04:20 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+var fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log =`${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append server.log');
+        }
+    });
+    console.log(`${now}: ${req.method} ${req.url}`);
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public')); 
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello express !<h1>');
+    // res.send({
+    //     name: 'Guyve',
+    //     likes: ['running', 'cinema']
+    // })
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        // currentYear: new Date().getFullYear(),
+        welcome: 'Bienvenue'
+    })
+});
+
+app.get('/about', (req, res) => {
+    // res.send('<h1>About Page<h1>');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        // currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMsg: 'Bad request ^^'
+    })
+});
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+});
